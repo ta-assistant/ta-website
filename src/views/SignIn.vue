a639411a1a237a88be426ed326df5a68c8e024c0
Siratee K <sirateek@gmail.com>
2021-05-26 14:18:02 +0700

    :sparkles: Add firebaseUi and implement the signIn page

---
+++ src/views/SignIn.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="signIn">
+    <p>Test</p>
+  </div>
+</template>
+
+<script>
+import { Vue } from "vue-property-decorator";
+
+export default class signIn extends Vue {}
+</script>
+
+<style scoped></style>
