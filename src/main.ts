63d54990ae8f4a6b7c79d9d7aef19607c1339542
Siratee K <sirateek@gmail.com>
2021-05-27 15:12:59 +0700

    :memo: Comment thje signIn path out durin the export template process

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,23 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import VueMaterial from "vue-material";
-import firebase from "firebase";
+// TODO: Uncomment these code after the export from template is completed
+// import firebase from "firebase";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
 
 // Export the firebaseConfig From Firebase Console by creating a new app
 // and copy the Firebase Web app configuration and paste it into the file named
 // firebaseConfig.json
-import firebaseConfig from "./firebaseConfig.json";
+// TODO: Uncomment these code after the export from template is completed
+// import firebaseConfig from "./firebaseConfig.json";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueMaterial);
 
-firebase.initializeApp(firebaseConfig);
+// TODO: Uncomment these code after the export from template is completed
+// firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
