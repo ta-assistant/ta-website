b32378d22edfeb1509d5a7e6df2f81190f1ba346
pontakornth <pontakorn61@gmail.com>
2021-06-03 14:34:44 +0700

    feat: Add exercise component

--- src/views/course/detail.vue
+++ src/views/course/detail.vue
@@ -12,6 +12,15 @@
             New Announcement
         </md-button>
     </div>
+        <div class="work-list">
+            <div class="work md-elevation-3 md-layout">
+                <p class="md-layout-item md-small-size-30">Exercise 1</p>
+                <md-progress-bar class="md-layout-item md-small-size-40" :md-value="40"></md-progress-bar>
+                <div class="md-layout-item md-small-size-30">
+                    <md-button class="md-primary md-raised">View Work</md-button>
+                </div>
+            </div>
+        </div>
     </div>
 </layout>
 </template>
@@ -32,11 +41,22 @@ export default Vue.extend({
 }
 .menu {
     display: flex;
+    padding: 0;
 }
 .menu > * {
     margin-right: .5rem;
+    margin-left: 0;
 }
 .container {
     padding: 1rem;
+}
+.work-list {
+    padding-top: 1rem;
+}
+.work {
+    padding: 1rem;
+}
+.md-progress-bar {
+    align-self: center;
 }
 </style>
