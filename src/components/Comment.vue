870b7f52d9ae11aad35379ec519b260ad5be71f9
pontakornth <pontakorn61@gmail.com>
2021-06-17 22:25:15 +0700

    feat: Make it render markdown

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -4,24 +4,25 @@
       <div class="md-title">Pontakorn Peasaeng</div>
     </md-card-header>
     <md-card-content>
-      <markdown-it-vue class="md-body" :content="comment"></markdown-it-vue>
+      <div v-html="htmlComment"></div>
     </md-card-content>
   </md-card>	
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import markdownItVue from "markdown-it-vue"
-import 'markdown-it-vue/dist/markdown-it-vue.css'
+import markdown from "../utils/markdown"
 export default Vue.extend({
   name: "Comment",
-  components: {
-    markdownItVue
-  },
   data: () => {
     return {
-      comment: "This is markdown *b* **mine**"
+      comment: "This is not fine. **Remember**"
     }
   },
+  computed: {
+    htmlComment() {
+      return markdown.render(this.comment)
+    }
+  }
 })
 </script>
