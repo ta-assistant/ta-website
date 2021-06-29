ad3778803c02debdc13c55ca37ff0b36ede048eb
Siratee K <sirateek@gmail.com>
2021-06-29 16:05:36 +0700

    Add types folder to keep all types file

---
+++ src/types/ClassroomAPI/courseWork.ts
@@ -0,0 +1,24 @@
+export type CourseWork = {
+  courseId: string;
+  id: string;
+  title: string;
+  description: string;
+  materials: Array<any>;
+  state: string;
+  alternateLink: string;
+  creationTime: string;
+  updateTime: string;
+  dueDate: string;
+  dueTime: string;
+  scheduledTime: string;
+  maxPoints: number;
+  workType: string;
+  associatedWithDeveloper: boolean;
+  assigneeMode: string;
+  individualStudentsOptions: any;
+  submissionModificationMode: string;
+  creatorUserId: string;
+  topicId: string;
+  assignment: any;
+  multipleChoiceQuestion: any;
+};
