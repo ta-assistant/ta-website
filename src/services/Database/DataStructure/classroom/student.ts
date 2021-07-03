9ecc536aa06f21fca8a7584927b4b56a1e28fbcb
Siratee K <sirateek@gmail.com>
2021-07-03 15:14:30 +0700

    :sparkles: Add the function to get the student and search the student from database

--- src/services/Database/DataStructure/classroom/student.ts
+++ src/services/Database/DataStructure/classroom/student.ts
@@ -1,5 +1,6 @@
 import { Database } from "../database";
 import firebase from "firebase";
+import { IDs } from "./teacher";
 
 export class Student extends Database {
   classroomId: string;
@@ -7,6 +8,61 @@ export class Student extends Database {
   constructor(firestore: firebase.firestore.Firestore, classroomId: string) {
     super(firestore);
     this.classroomId = classroomId;
+  }
+
+  get(by: IDs): Promise<firebase.firestore.DocumentSnapshot> {
+    if (typeof by.userId === "undefined") {
+      throw Error("To get the specific student. Please speficied the userId");
+    }
+    return this.getFirestore()
+      .collection("Classrooms")
+      .doc(this.classroomId)
+      .collection("students")
+      .doc(by.userId)
+      .get();
+  }
+
+  search(by: IDs): Promise<firebase.firestore.QuerySnapshot> {
+    if (typeof by === "undefined") {
+      throw Error("Please specified by param");
+    }
+    const firestoreCollection = this.getFirestore()
+      .collection("Classrooms")
+      .doc(this.classroomId)
+      .collection("students");
+    let firestoreSearch: firebase.firestore.Query | null = null;
+
+    if (typeof by.classroomUserId !== "undefined") {
+      firestoreSearch = firestoreCollection.where(
+        "classroomUserId",
+        "==",
+        by.classroomUserId
+      );
+    }
+
+    if (typeof by.studentId !== "undefined") {
+      if (firestoreSearch === null) {
+        firestoreSearch = firestoreCollection.where(
+          "studentId",
+          "==",
+          by.studentId
+        );
+      } else {
+        firestoreSearch = firestoreSearch.where(
+          "studentId",
+          "==",
+          by.studentId
+        );
+      }
+    }
+
+    if (firestoreSearch === null) {
+      throw Error(
+        "To search student, Please specified either `classroomUserId` or `studentId`"
+      );
+    }
+
+    return firestoreSearch.get();
   }
 
   list(): Promise<firebase.firestore.QuerySnapshot> {
