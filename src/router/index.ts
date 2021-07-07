ae1db0a464167bd4d2f60ce7a405cbcf95dcce7f
pontakornth <pontakorn61@gmail.com>
2021-07-07 14:40:43 +0700

    feat: Add context for breadcumb component

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,40 +9,61 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
+    meta: {
+      breadcumb: "Home"
+    },
     component: Home,
   },
   {
     path: "/signIn",
     name: "SignIn",
+    meta: {
+      breadcumb: "Sign In"
+    }
     component: SignIn,
   },
   {
     path: "/course",
     name: "course",
+    meta: {
+      breadcumb: "Course",
+    },
     component: () =>
       import(/* webpackChunkName: "course" */ "@/views/course.vue"),
   },
   {
     path: "/course/:courseId",
     name: "course_detail",
+    meta: {
+      breadcumb: "Course Detail"
+    },
     component: () =>
       import(/* webpackChunkName: "course" */ "@/views/course/detail.vue"),
   },
   {
     path: "/course/:courseId/work/:workId",
     name: "WorkSubmission",
+    meta: {
+      breadcumb: "Work Submission"
+    },
     component: () =>
       import(/* webpackChunkName: "work" */ "@/views/course/submission.vue"),
   },
   {
     path: "/course/:courseId/work/:workId/submission/:submissionId",
     name: "exercise",
+    meta: {
+      breadcumb: "Submission Detail"
+    },
     component: () =>
       import(/* webpackChunkName: "work" */ "@/views/course/work.vue"),
   },
   {
     path: "/about",
     name: "About",
+    meta: {
+      breadcumb: "About"
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -52,6 +73,9 @@ const routes: Array<RouteConfig> = [
   {
     path: "/settings",
     name: "Settings",
+    meta: {
+      breadcumb: "Settings"
+    },
     component: () =>
       import(/* webpackChunkName: "setting" */ "../views/settings.vue"),
   },
