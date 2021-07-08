ffcf15bdddcd7992f150228a6f6a6b355cc04d73
Siratee K <sirateek@gmail.com>
2021-07-08 16:39:18 +0700

    Update the type to be undefinable and disable eslint to check this file

--- src/types/ClassroomAPI/courses.ts
+++ src/types/ClassroomAPI/courses.ts
@@ -1,28 +1,28 @@
+/* eslint-disable */
 export enum CourseState {
-  COURSE_STATE_UNSPECIFIED,
-  ACTIVE,
-  ARCHIVED,
-  PROVISIONED,
-  DECLINED,
-  SUSPENDED,
+  active = "ACTIVE",
+  archived = "ARCHIVED",
+  provisioned = "PROVISIONED",
+  declined = "DECLINED",
+  suspended = "SUSPENDED",
 }
 export type Course = {
-  id: string;
-  name: string;
-  section: string;
-  descriptionHeading: string;
-  description: string;
-  room: string;
-  ownerId: string;
-  creationTime: string;
-  updateTime: string;
-  enrollmentCode: string;
-  courseState: CourseState;
-  alternateLink: string;
-  teacherGroupEmail: string;
-  courseGroupEmail: string;
-  teacherFolder: any;
-  courseMaterialSets: any;
-  guardiansEnabled: boolean;
-  calendarId: string;
+  id?: string;
+  name?: string;
+  section?: string;
+  descriptionHeading?: string;
+  description?: string;
+  room?: string;
+  ownerId?: string;
+  creationTime?: string;
+  updateTime?: string;
+  enrollmentCode?: string;
+  courseState?: CourseState;
+  alternateLink?: string;
+  teacherGroupEmail?: string;
+  courseGroupEmail?: string;
+  teacherFolder?: any;
+  courseMaterialSets?: any;
+  guardiansEnabled?: boolean;
+  calendarId?: string;
 };
